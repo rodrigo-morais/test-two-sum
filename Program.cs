aad872034c91dbe2e0d7bafce0e86f1d698cf50d
Rodrigo Morais <morais.rm@gmail.com>
2015-08-18 09:55:09 -0300

    Adding solution with not best performance.

--- Program.cs
+++ Program.cs
@@ -1,15 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace test_two_sum
+class TwoSum
 {
-    class Program
+    public static Tuple<int, int> FindTwoSum(IList<int> list, int sum)
     {
-        static void Main(string[] args)
+        Tuple<int, int> sums = null;
+        var find = false;
+        var count = 0;
+        var middle = list.Count > 0 ? ((list.Count / 2) + 1) : 0;
+
+        while (count < middle && !find)
         {
+            var second = sum - list[count];
+            var previous = sum - list[list.Count - count - 1];
+
+            second = list.IndexOf(second);
+            previous = list.IndexOf(previous);
+
+            if (second > -1)
+            {
+                find = true;
+                sums = Tuple.Create(count, second);
+            }
+            else if (previous > -1)
+            {
+                find = true;
+                sums = Tuple.Create(list.Count - count - 1, previous);
+            }
+
+            count++;
         }
+
+        return sums;
+    }
+
+    public static void Main(string[] args)
+    {
+        Tuple<int, int> indices = FindTwoSum(new List<int>() { 1, 3, 5, 7, 9 }, 12);
+        Console.WriteLine(indices.Item1 + " " + indices.Item2);
     }
 }
