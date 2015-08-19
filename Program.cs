7d6190fa8a016546988b69864488307ae380974d
Rodrigo Morais <morais.rm@gmail.com>
2015-08-19 11:57:01 -0300

    Improving the solution.

--- Program.cs
+++ Program.cs
@@ -1,43 +1,21 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 class TwoSum
 {
     public static Tuple<int, int> FindTwoSum(IList<int> list, int sum)
     {
-        Tuple<int, int> sums = null;
-        var find = false;
-        var count = 0;
-        var middle = list.Count > 0 ? ((list.Count / 2) + 1) : 0;
-
-        while (count < middle && !find)
-        {
-            var second = sum - list[count];
-            var previous = sum - list[list.Count - count - 1];
-
-            second = list.IndexOf(second);
-            previous = list.IndexOf(previous);
-
-            if (second > -1)
-            {
-                find = true;
-                sums = Tuple.Create(count, second);
-            }
-            else if (previous > -1)
-            {
-                find = true;
-                sums = Tuple.Create(list.Count - count - 1, previous);
-            }
-
-            count++;
-        }
-
-        return sums;
+        var newList = list.ToArray();
+        var pairs = (from number in list where newList.Contains(sum - number) select Tuple.Create(Array.IndexOf(newList, number), Array.LastIndexOf(newList, sum - number))).ToList();
+        
+        return pairs.Count > 0 ? pairs[0] : null;
     }
 
     public static void Main(string[] args)
     {
-        Tuple<int, int> indices = FindTwoSum(new List<int>() { 1, 3, 5, 7, 9 }, 12);
+        Tuple<int, int> indices = FindTwoSum(new List<int>() { 6, 1, 3, 5, 6, 7, 9 }, 12);
         Console.WriteLine(indices.Item1 + " " + indices.Item2);
     }
 }
